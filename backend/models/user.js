7be96aaadc81b766ba2ad100434a29672bcc16da
Salima Ait Kassi <salyma.salama@gmail.com>
2022-05-19 03:10:15 +0200

    parcours user

---
+++ backend/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { stringify } = require('querystring');
+
+const userSchema = mongoose.Schema({
+    email: { type: string, required: true },
+    password: { type: string, required: true }
+
+})
+
+module.exports = mongoose.model('user', userSchema);
